8f6352f79afe862720bf1f970b1b09b419d5e1a3
Austin <austin.saco2@gmail.com>
2022-03-07 11:39:16 -0600

    added simple form gem, migrated db, created form for posting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,17 @@ class PostsController < ApplicationController
     end
 
     def new
+        @post = Post.new
     end
 
     def create
+        @post = Post.new()
+    end
+
+    private 
+
+    def post_params
+        params.require(:post).permit(:title, :content)
     end
     
 end
